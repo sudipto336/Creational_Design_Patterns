91d37cb39cd85fc21d52e17b2082a1ccb973e297
Sudipto Saha <sahasudipto336@gmail.com>
2024-01-23 18:37:54 +0530

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleThreadedSingleton singleThreadedSingleton1 = SingleThreadedSingleton.GetInstance();
+            singleThreadedSingleton1.printMessage("message1");
+            SingleThreadedSingleton singleThreadedSingleton2 = SingleThreadedSingleton.GetInstance();
+            singleThreadedSingleton2.printMessage("message2");
+        }
+    }
+}
---
+++ Singleton/SingleThreadedSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class SingleThreadedSingleton
+    {
+        private static SingleThreadedSingleton instance;
+
+        private int counter = 0;
+
+        private SingleThreadedSingleton() {
+            counter++;
+            Console.WriteLine($"Counter value increased");
+            Console.WriteLine($"Counter value - {counter}");
+        }
+
+        public static SingleThreadedSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SingleThreadedSingleton();
+            }
+            return instance;
+        }
+
+        public void printMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
