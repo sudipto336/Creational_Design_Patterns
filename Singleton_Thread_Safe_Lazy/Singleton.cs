cac0ac7faebb441a7229f1a7578e63c4c0716cc9
Sudipto Saha <sahasudipto336@gmail.com>
2024-01-26 09:37:40 +0530

    add thread safe singleton using Lazy<T>

---
+++ Singleton_Thread_Safe_Lazy/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Thread_Safe_Lazy
+{
+    internal sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+
+        private static int counter = 0;
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"Counter value increased");
+            Console.WriteLine($"Counter value - {counter}");
+        }
+
+        public static Singleton GetInstance()
+        {
+            return instance.Value;
+        }
+
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
