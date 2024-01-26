3bce513e87dae0baf258d5d64e64d48764e0f7b3
Sudipto Saha <sahasudipto336@gmail.com>
2024-01-26 09:32:42 +0530

    add thred safe singleton using double check lock

---
+++ Singleton_Thread_Safe_Double_Check_Locking/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton_Thread_Safe_Double_Check_Locking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => CallSingletonMethod(),
+                () => CallSingletonMethod()
+            );
+        }
+
+        private static void CallSingletonMethod()
+        {
+            Singleton singleton = Singleton.GetInstance();
+            singleton.PrintMessage("message");
+        }
+    }
+}
---
+++ Singleton_Thread_Safe_Double_Check_Locking/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Thread_Safe_Double_Check_Locking
+{
+    internal sealed class Singleton
+    {
+        private static Singleton instance;
+        private static readonly Object lockInstance = new Object();
+
+        private static int counter = 0;
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"Counter value increased");
+            Console.WriteLine($"Counter value - {counter}");
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockInstance)
+                {
+                    if (instance == null)
+                    {
+
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
