c8e9a87f34ce407fa432cd203ffcefcfaa6a9b17
Sudipto Saha <sahasudipto336@gmail.com>
2024-01-26 09:26:20 +0530

    add thread safe singleton using single check lock

---
+++ Singleton_Thread_Safe_Single_Check_Locking/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Thread_Safe_Single_Check_Locking
+{
+    internal sealed class Singleton
+    {
+        private static Singleton instance;
+        private static readonly Object lockInstance = new Object();
+
+        private static int counter = 0;
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"Counter value increased");
+            Console.WriteLine($"Counter value - {counter}");
+        }
+
+        public static Singleton GetInstance()
+        {
+            lock (lockInstance) { 
+                if (instance == null)
+                {
+
+                    instance = new Singleton();
+                }
+            }
+            return instance;
+        }
+
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
