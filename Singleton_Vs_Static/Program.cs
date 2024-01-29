eb75bf2f04c3388b450c3b4d4733575ebf0c83dd
Sudipto Saha <sahasudipto336@gmail.com>
2024-01-29 22:10:59 +0530

    Add static vs singleton

--- Singleton_Vs_Static/Program.cs
+++ Singleton_Vs_Static/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please select option to convertor direction");
+            Console.WriteLine("1. From Celsius to Farhenheit");
+            Console.WriteLine("2. From Farhenheit to Celsius");
+            string selection = Console.ReadLine();
+            switch (selection) {
+                case "1":
+                    Console.Write("Enter temperature in Celsius: ");
+                    Console.WriteLine("Temperature in Farhenheit: {0:F2}", TemperatureConverter.ConvertCelsiusToFarhenheit(Console.ReadLine()));
+                    break;
+                case "2":
+                    Console.Write("Enter temperature in Farhenheit: ");
+                    Console.WriteLine("Temperature in Celsius: {0:F2}", TemperatureConverter.ConvertFarhenheitToCelsius(Console.ReadLine()));
+                    break;
+                default:
+                    Console.WriteLine("Please select option to convertor direction");
+                    break;
+
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
---
+++ Singleton_Vs_Static/TemperatureConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Vs_Static
+{
+    internal class TemperatureConverter
+    {
+        public static double ConvertCelsiusToFarhenheit(string temperatureInCelsius)
+        {
+            return (Double.Parse(temperatureInCelsius) * 9 / 5) + 32;
+        }
+
+        public static double ConvertFarhenheitToCelsius(string temperatureInFarhenheit)
+        {
+            return (Double.Parse(temperatureInFarhenheit) * 5 / 9) - 32;
+        }
+    }
+}
