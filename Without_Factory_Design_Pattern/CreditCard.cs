7e7a5e5116304d1b753a46127dc65499704bf727
Sudipto Saha <sahasudipto336@gmail.com>
2024-02-01 23:47:02 +0530

    Add without factory design pattern

---
+++ Without_Factory_Design_Pattern/CreditCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Without_Factory_Design_Pattern
+{
+    internal interface CreditCard
+    {
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+}
---
+++ Without_Factory_Design_Pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Without_Factory_Design_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter card name: ");
+            string cardType = Console.ReadLine();
+
+            CreditCard card;
+
+            if(cardType.Equals("MoneyBack"))
+            {
+                card = new MoneyBack();
+            } else if (cardType.Equals("Platinum"))
+            {
+                card = new Platinum();
+            } else
+            {
+                card = new Titanium();
+            }
+
+            Console.WriteLine($"Card annual charge: {card.GetAnnualCharge()}");
+            Console.WriteLine($"Card credit limit: {card.GetCreditLimit()}");
+        }
+    }
+}
