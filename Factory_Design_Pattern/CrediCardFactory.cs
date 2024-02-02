331a0da060616a9e13330c8334d46010bbaebea7
Sudipto Saha <sahasudipto336@gmail.com>
2024-02-02 07:25:33 +0530

    Add factory design pattern

---
+++ Factory_Design_Pattern/CrediCardFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Design_Pattern
+{
+    internal class CrediCardFactory
+    {
+        public static CreditCard GetCreditCard(string cardName)
+        {
+            CreditCard card = null;
+
+            if (cardName.Equals("MoneyBack"))
+            {
+                card = new MoneyBack();
+            }
+            else if (cardName.Equals("Platinum"))
+            {
+                card = new Platinum();
+            }
+            else
+            {
+                card = new Titanium();
+            }
+            return card;
+        }
+    }
+}
---
+++ Factory_Design_Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Factory_Design_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter card name: ");
+            string cardName = Console.ReadLine();
+
+            CreditCard card = CrediCardFactory.GetCreditCard(cardName);
+            Console.WriteLine($"Card annual charge: {card.GetAnnualCharge()}");
+            Console.WriteLine($"Card credit limit: {card.GetCreditLimit()}");
+        }
+    }
+}
