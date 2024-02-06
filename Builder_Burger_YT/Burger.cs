4c8ac11470b43ee67d01229b296b59d4475772f9
Sudipto Saha <sahasudipto336@gmail.com>
2024-02-06 07:39:24 +0530

    Add builder pattern

---
+++ Builder_Burger_YT/Burger.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Burger_YT
+{
+    internal class Burger
+    {
+        public string Size { get; }
+        public bool Mayonese { get; }
+        public bool ExtraCheese { get; }
+        public bool Sauce { get; }
+        public bool Lettuce { get; }
+        public bool Onion { get; }
+
+        private Burger(BurgerBuilder builder)
+        {
+            this.Onion = builder.GetOnion();
+            this.Lettuce = builder.GetLettuce();
+            this.Sauce = builder.GetSauce();
+            this.Onion = builder.GetOnion();
+            this.Size = builder.GetSize();
+            this.ExtraCheese = builder.GetExtraCheese();
+        }
+
+        internal class BurgerBuilder
+        {
+            private string size;
+            private bool mayonese;
+            private bool extraCheese;
+            private bool sauce;
+            private bool lettuce;
+            private bool onion;
+
+            public BurgerBuilder SetSize(string size)
+            {
+                this.size = size;
+                return this;
+            }
+
+            public BurgerBuilder SetMayonese(bool mayonese)
+            {
+                this.mayonese = mayonese;
+                return this;
+            }
+
+            public BurgerBuilder SetExtraCheese(bool extraCheese)
+            {
+                this.extraCheese = extraCheese;
+                return this;
+            }
+
+            public BurgerBuilder SetSauce(bool sauce)
+            {
+                this.sauce = sauce;
+                return this;
+            }
+
+            public BurgerBuilder SetLettuce(bool lettuce)
+            {
+                this.lettuce = lettuce;
+                return this;
+            }
+
+            public BurgerBuilder SetOnion(bool onion)
+            {
+                this.onion = onion;
+                return this;
+            }
+
+            public string GetSize()
+            {
+                return this.size;
+            }
+
+            public bool GetMayonese()
+            {
+                return this.mayonese;
+            }
+
+            public bool GetSauce()
+            {
+                return this.sauce;
+            }
+
+            public bool GetLettuce()
+            {
+                return this.lettuce;
+            }
+
+            public bool GetOnion()
+            {
+                return this.onion;
+            }
+
+            public bool GetExtraCheese()
+            {
+                return this.extraCheese;
+            }
+
+            public Burger BuildBurger()
+            {
+                return new Burger(this);
+            }
+        }
+    }
+}
---
+++ Builder_Burger_YT/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Builder_Burger_YT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Burger burger = new Burger.BurgerBuilder().SetSize("large").SetSauce(true).SetLettuce(true).SetExtraCheese(false).SetOnion(true).SetMayonese(false).BuildBurger();
+            string burgerSize = $"Burger size: {burger.Size}";
+            string isSauceAdded = burger.Sauce ? "Sauce added in the burger" : "Sauce is not added in the burger";
+            string isMayoneseAdded = burger.Mayonese ? "Mayonese added in the burger" : "Mayonese is not added in the burger";
+            string isOnionAdded = burger.Onion ? "Onion added in the burger" : "Onion is not added in the burger";
+            string isExtraCheeseAdded = burger.ExtraCheese ? "Extra cheese added in the burger" : "Extra cheese is not added in the burger";
+            string isLettuceAdded = burger.Lettuce ? "Lettuce added in the burger" : "Lettuce is not added in the burger";
+            Console.WriteLine($"{burgerSize}");
+            Console.WriteLine($"{isSauceAdded}");
+            Console.WriteLine($"{isMayoneseAdded}");
+            Console.WriteLine($"{isOnionAdded}");
+            Console.WriteLine($"{isExtraCheeseAdded}");
+            Console.WriteLine($"{isLettuceAdded}");
+        }
+    }
+}
